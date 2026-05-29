6093703ac47ae7485aa0c1508f6844c2c5c4c1f4
Neil Xia <neil.l.xia@gmail.com>
2026-05-29 14:39:21 -0700

    fix init file to remove unnecessary parts

--- src/rater_example/__init__.py
+++ src/rater_example/__init__.py
@@ -1,9 +1,7 @@
-from rater_example.rating_engine import Rater, RiskInput, PremiumResult
+from rater_example.rating_engine import Rater
 from rater_example.rating_tables import RatingTables
 
 __all__ = [
     "Rater",
-    "RiskInput",
-    "PremiumResult",
     "RatingTables",
 ]
